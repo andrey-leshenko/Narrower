8a486f9cc7b58eacee96e206f8d46ea9c0f7307b
Andrey Leshenko <andrey.leshenko@gmail.com>
2024-10-04 02:17:18 +0300

    Update to manifest v3

--- src_chrome/popup.js
+++ src_chrome/popup.js
@@ -3,37 +3,51 @@ var DEFAULT_PADDING = 20;
 var slider = document.getElementById('slider');
 var sliderLabel = document.getElementById('slider-value');
 
-function setSliderLabel(value) {
-    sliderLabel.textContent = value * 2 + '%';
-}
-
-function setHorizontalMargin(value) {
-    chrome.tabs.executeScript(
-    {code:
-     'document.documentElement.style.paddingLeft="' + value + '%";'
-     + 'document.documentElement.style.paddingRight="' + value + '%";'
-    });
-}
-
-chrome.tabs.executeScript(
-    {code: 'localStorage.getItem("narrowerExtention_pagePadding")'},
-    function(result) {
-    var startPadding = isNaN(parseInt(result)) ? DEFAULT_PADDING : result;
-    
-    setHorizontalMargin(startPadding);
-    
-    slider.value = startPadding;
-    setSliderLabel(startPadding);
-    });
+var currTabId = (await chrome.tabs.query({active: true, currentWindow: true}))[0].id;
 
 slider.addEventListener('input', function() {
-    setHorizontalMargin(this.value);
-    setSliderLabel(this.value);
+    chrome.scripting.executeScript({
+        target: {tabId: currTabId},
+        func: remote_setHorizontalMargin,
+        args: [this.value]
+    });
+    sliderLabel.textContent = this.value * 2 + '%';
 });
 
 slider.addEventListener('change', function() {
-    chrome.tabs.executeScript(
-    {code:
-     'localStorage.setItem("narrowerExtention_pagePadding", ' + this.value + ')'
+    chrome.scripting.executeScript({
+        target: {tabId: currTabId},
+        func: remote_setSavedPadding,
+        args: [this.value]
     });
 });
+
+var savedPadding = await getSavedPadding();
+var startPadding =  isNaN(parseInt(savedPadding)) ? DEFAULT_PADDING : savedPadding;
+slider.value = startPadding;
+slider.dispatchEvent(new Event('input'));
+
+// Local functions
+
+async function getSavedPadding() {
+    var results = await chrome.scripting.executeScript({
+        target: {tabId: currTabId},
+        func: remote_getSavedPadding,
+    });
+    return results[0].result;
+}
+
+// Remote functions
+
+function remote_setHorizontalMargin(value) {
+    document.documentElement.style.paddingLeft = value.toString() + '%';
+    document.documentElement.style.paddingRight = value.toString() + '%';
+}
+
+function remote_getSavedPadding() {
+    return localStorage.getItem('narrowerExtention_pagePadding');
+}
+
+function remote_setSavedPadding(value) {
+    localStorage.setItem('narrowerExtention_pagePadding', value);
+}
